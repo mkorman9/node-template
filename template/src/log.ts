4a1ae9dcd8cc70bd659235461ad08af155eeb08d
Michal Korman <michal.korman@icloud.com>
2023-07-28 21:48:53 +0200

    fix log format

--- template/src/log.ts
+++ template/src/log.ts
@@ -4,9 +4,7 @@ export default winston.createLogger({
   level: 'info',
   transports: [new winston.transports.Console()],
   format: winston.format.combine(
-    winston.format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss.SSS'
-    }),
+    winston.format.timestamp(),
     winston.format.printf(info => {
       if (info.stack) {
         return `${info.timestamp} | ${info.level} | ${info.message}: ${info.stack}`;
