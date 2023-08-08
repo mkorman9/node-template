3a0e66d5f45df3db0edef37c0df5a5b63376d90e
Michal Korman <michal.korman@icloud.com>
2023-08-08 21:40:10 +0200

    simplify files

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 Creates Node app from scratch in the current directory. Usage:
 
 ```sh
-npm install -g typescript-node-app@1.4.2
+npm install -g typescript-node-app@1.4.3
 npx typescript-node-app
 ```
--- template/src/index.ts
+++ template/src/index.ts
@@ -1,8 +1,18 @@
 import './config';
-import './error_handlers';
-
-
+import log from './log';
 
 process.on('SIGINT', () => {
   process.exit(0);
+});
+
+process.on('uncaughtException', err => {
+  log.error('Unhandled exception', { stack: err.stack });
+});
+
+process.on('unhandledRejection', reason => {
+  if (reason instanceof Error) {
+    log.error(`Unhandled Promise rejection`, { stack: reason.stack });
+  } else {
+    log.error(`Unhandled Promise rejection: ${reason}`);
+  }
 });
