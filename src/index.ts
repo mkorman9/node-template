2305d4fcfab900b2d476422c3d279200437f9a65
Michal Korman <michal.korman@icloud.com>
2023-12-19 19:36:56 +0100

    refactor index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,33 @@
 import config from './config';
 
-import {startServer} from './http/server';
+import {ServerProcess, startServer} from './http/server';
 import app from './app';
 
 async function shutdown() {
   
 }
 
+async function stopServer(server: ServerProcess) {
+  try {
+    await server.stop();
+    console.log('⛔ Server has stopped');
+  } catch (e) {
+    console.log(`🚫 Error while stopping the server: ${e instanceof Error ? e.stack : e}`);
+  }
+
+  try {
+    await shutdown();
+  } catch (e) {
+    console.log(`🚫 Error during shutdown: ${e instanceof Error ? e.stack : e}`);
+  }
+
+  process.exit(0);
+}
+
 startServer(app, config.HTTP_HOST, config.HTTP_PORT)
   .then(server => {
     console.log(`✅ Server started on ${server.address}`);
-
-    process.on('SIGINT', async () => {
-      try {
-        await server.stop();
-        console.log('⛔ Server has stopped');
-      } catch (e) {
-        console.log(`🚫 Error while stopping the server: ${e instanceof Error ? e.stack : e}`);
-      }
-
-      try {
-        await shutdown();
-      } catch (e) {
-        console.log(`🚫 Error during shutdown: ${e instanceof Error ? e.stack : e}`);
-      }
-
-      process.exit(0);
-    });
+    process.on('SIGINT', stopServer);
   })
   .catch(err => {
     console.log(`🚫 Failed to start the server: ${err.stack}`);
