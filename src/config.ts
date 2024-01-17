2cd2dfbf5f41274af74365feec642a80d927e890
Michal Korman <michal.korman@icloud.com>
2024-01-17 12:02:25 +0100

    simplify config loading

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,9 @@
 import 'dotenv/config';
 import {bool, cleanEnv, port, str} from 'envalid';
 
-const envs = {
+export default cleanEnv(process.env, {
   HTTP_HOST: str({default: '0.0.0.0'}),
   HTTP_PORT: port({default: 8080}),
   HTTP_CORS_ORIGIN: str({default: undefined}),
   HTTP_TRUST_PROXIES: bool({default: true})
-};
-
-export default cleanEnv(process.env, envs);
+});
