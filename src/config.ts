c65dcae4fdf172532a689098ea55fc5edcd48bea
Michal Korman <michal.korman@icloud.com>
2024-01-17 01:10:17 +0100

    simplify HTTP_TRUST_PROXIES parsing

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ const ConfigSchema = z.object({
   HTTP_HOST: z.string().default('0.0.0.0'),
   HTTP_PORT: z.coerce.number().int().default(8080),
   HTTP_CORS_ORIGIN: z.string().optional(),
-  HTTP_TRUST_PROXIES: z.string().transform(v => JSON.parse(v)).pipe(z.boolean())
+  HTTP_TRUST_PROXIES: z.string().transform(v => JSON.parse(v)).pipe(z.boolean()).default('true')
 });
 
 export default (() => {
