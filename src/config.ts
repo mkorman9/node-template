bb2af7f6f64f0d49e6240e399bd3de5d27829329
Michal Korman <michal.korman@icloud.com>
2024-01-23 12:59:03 +0100

    listen on ipv6 interface by default

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import {bool, cleanEnv, port, str} from 'envalid';
 
 export default cleanEnv(process.env, {
-  HTTP_HOST: str({default: '0.0.0.0'}),
+  HTTP_HOST: str({default: '::'}),
   HTTP_PORT: port({default: 8080}),
   HTTP_CORS_ORIGIN: str({default: undefined}),
   HTTP_TRUST_PROXIES: bool({default: true})
