505af081fc378f8d809024d0153b8d20e4641e93
Michal Korman <michal.korman@icloud.com>
2024-01-27 21:26:59 +0100

    add validateParams

--- src/http/validation.ts
+++ src/http/validation.ts
@@ -1,19 +1,18 @@
 import express, {RequestHandler} from 'express';
-import {ParamsDictionary} from 'express-serve-static-core';
 import z from 'zod';
 
 const jsonMiddleware = express.json();
 
 export function validateBody<TSchema extends z.Schema>(
   schema: TSchema
-): RequestHandler<ParamsDictionary, unknown, z.TypeOf<TSchema>, unknown> {
+): RequestHandler<unknown, unknown, z.TypeOf<TSchema>, unknown> {
   return (req, res, next) => {
     jsonMiddleware(req, res, (err?: Error) => {
       if (err) {
         return res.status(400).json({
           title: 'Provided request body cannot be parsed',
           type: 'MalformedRequestBody',
-          cause: process.env.NODE_ENV === 'production' ? undefined : err.stack
+          cause: process.env.NODE_ENV === 'production' ? undefined : (err.stack)
         });
       }
 
@@ -35,9 +34,30 @@ export function validateBody<TSchema extends z.Schema>(
   };
 }
 
-export function validateQueryParams<TSchema extends z.Schema>(
+export function validateParams<TSchema extends z.Schema>(
   schema: TSchema
-): RequestHandler<ParamsDictionary, unknown, unknown, z.TypeOf<TSchema>> {
+): RequestHandler<z.TypeOf<TSchema>, unknown, unknown, unknown> {
+  return (req, res, next) => {
+    const result = schema.safeParse(req.params);
+    if (!result.success) {
+      return res.status(400).json({
+        title: 'Provided request path parameters contain schema violations',
+        type: 'ValidationError',
+        cause: result.error.issues.map(issue => ({
+          location: issue.path,
+          code: issue.message.toLowerCase() === 'required' ? 'required' : issue.code,
+          message: issue.message
+        }))
+      });
+    }
+
+    next();
+  };
+}
+
+export function validateQuery<TSchema extends z.Schema>(
+  schema: TSchema
+): RequestHandler<unknown, unknown, unknown, z.TypeOf<TSchema>> {
   return (req, res, next) => {
     const result = schema.safeParse(req.query);
     if (!result.success) {
