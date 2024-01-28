cc59b160b2101ee03270125e1e25b5378b8c7753
Michal Korman <michal.korman@icloud.com>
2024-01-28 19:02:19 +0100

    implement authenticateToken

---
+++ src/http/authentication.ts
@@ -0,0 +1,35 @@
+import {RequestHandler} from 'express';
+
+export type AuthLocals<T> = {
+  auth: T;
+  [key: string]: any;  // eslint-disable-line @typescript-eslint/no-explicit-any
+};
+
+export function authenticateToken<TParams, TQuery, TBody, TAuth>(
+  handler: (token: string) => Promise<TAuth>
+): RequestHandler<TParams, unknown, TBody, TQuery, AuthLocals<TAuth>> {
+  return (req, res, next) => {
+    const authorizationHeader = req.header('Authorization') || '';
+    const bearer = 'bearer ';
+    if (authorizationHeader.substring(0, bearer.length).toLowerCase() != bearer) {
+      return res.status(401).json({
+        title: 'Resource requires providing authorization data',
+        type: 'MissingAuthorization'
+      });
+    }
+
+    const token = authorizationHeader.substring(bearer.length).trim();
+
+    handler(token)
+      .then(auth => {
+        res.locals.auth = auth;
+        next();
+      })
+      .catch(() => {
+        res.status(403).json({
+          title: 'Provided authorization data is invalid',
+          type: 'AccessDenied'
+        });
+      });
+  };
+}
