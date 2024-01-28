c9e66d44856f9f3441e9e1a488cf1417a016315c
Michal Korman <michal.korman@icloud.com>
2024-01-28 01:08:07 +0100

    refactor validation

--- src/http/validation.ts
+++ src/http/validation.ts
@@ -1,27 +1,33 @@
 import express, {RequestHandler} from 'express';
 import z from 'zod';
 
-type TPlaceholder = Record<string, any>;  // eslint-disable-line @typescript-eslint/no-explicit-any
+type InferredType<T extends z.ZodRawShape> = z.TypeOf<z.ZodObject<T>>;
+
+export type RequestValidators<
+  TParamsShape extends z.ZodRawShape,
+  TQueryShape extends z.ZodRawShape,
+  TBodyShape extends z.ZodRawShape
+> = {
+  params?: TParamsShape,
+  query?: TQueryShape,
+  body?: TBodyShape
+};
 
 const jsonMiddleware = express.json();
 
-export function validateBody<TShape extends z.ZodRawShape>(
-  shape: TShape
-): RequestHandler<TPlaceholder, unknown, z.TypeOf<z.ZodObject<TShape>>, TPlaceholder> {
+export function validate<
+  TParamsShape extends z.ZodRawShape,
+  TQueryShape extends z.ZodRawShape,
+  TBodyShape extends z.ZodRawShape
+>(
+  validators: RequestValidators<TParamsShape, TQueryShape, TBodyShape>
+): RequestHandler<InferredType<TParamsShape>, unknown, InferredType<TBodyShape>, InferredType<TQueryShape>> {
   return (req, res, next) => {
-    jsonMiddleware(req, res, (err?: Error) => {
-      if (err) {
-        return res.status(400).json({
-          title: 'Provided request body cannot be parsed',
-          type: 'MalformedRequestBody',
-          cause: process.env.NODE_ENV === 'production' ? undefined : err.stack
-        });
-      }
-
-      const result = z.object(shape).safeParse(req.body);
+    if (validators.params) {
+      const result = z.object(validators.params).safeParse(req.params);
       if (!result.success) {
         return res.status(400).json({
-          title: 'Provided request body contains schema violations',
+          title: 'Provided request path parameters contain schema violations',
           type: 'ValidationError',
           cause: result.error.issues.map(issue => ({
             location: issue.path,
@@ -31,51 +37,53 @@ export function validateBody<TShape extends z.ZodRawShape>(
         });
       }
 
+      req.params = result.data;
+    }
+
+    if (validators.query) {
+      const result = z.object(validators.query).safeParse(req.query);
+      if (!result.success) {
+        return res.status(400).json({
+          title: 'Provided request query parameters contain schema violations',
+          type: 'ValidationError',
+          cause: result.error.issues.map(issue => ({
+            location: issue.path,
+            code: issue.message.toLowerCase() === 'required' ? 'required' : issue.code,
+            message: issue.message
+          }))
+        });
+      }
+
+      req.query = result.data;
+    }
+
+    if (validators.body) {
+      jsonMiddleware(req, res, (err?: Error) => {
+        if (err) {
+          return res.status(400).json({
+            title: 'Provided request body cannot be parsed',
+            type: 'MalformedRequestBody',
+            cause: process.env.NODE_ENV === 'production' ? undefined : err.stack
+          });
+        }
+
+        const result = z.object(validators.body!).safeParse(req.body);
+        if (!result.success) {
+          return res.status(400).json({
+            title: 'Provided request body contains schema violations',
+            type: 'ValidationError',
+            cause: result.error.issues.map(issue => ({
+              location: issue.path,
+              code: issue.message.toLowerCase() === 'required' ? 'required' : issue.code,
+              message: issue.message
+            }))
+          });
+        }
+
+        next();
+      });
+    } else {
       next();
-    });
-  };
-}
-
-export function validateParams<TShape extends z.ZodRawShape>(
-  shape: TShape
-): RequestHandler<z.TypeOf<z.ZodObject<TShape>>, unknown, TPlaceholder, TPlaceholder> {
-  return (req, res, next) => {
-    const result = z.object(shape).safeParse(req.params);
-    if (!result.success) {
-      return res.status(400).json({
-        title: 'Provided request path parameters contain schema violations',
-        type: 'ValidationError',
-        cause: result.error.issues.map(issue => ({
-          location: issue.path,
-          code: issue.message.toLowerCase() === 'required' ? 'required' : issue.code,
-          message: issue.message
-        }))
-      });
     }
-
-    req.params = result.data;
-    next();
-  };
-}
-
-export function validateQuery<TShape extends z.ZodRawShape>(
-  shape: TShape
-): RequestHandler<TPlaceholder, unknown, TPlaceholder, z.TypeOf<z.ZodObject<TShape>>> {
-  return (req, res, next) => {
-    const result = z.object(shape).safeParse(req.query);
-    if (!result.success) {
-      return res.status(400).json({
-        title: 'Provided request query parameters contain schema violations',
-        type: 'ValidationError',
-        cause: result.error.issues.map(issue => ({
-          location: issue.path,
-          code: issue.message.toLowerCase() === 'required' ? 'required' : issue.code,
-          message: issue.message
-        }))
-      });
-    }
-
-    req.query = result.data;
-    next();
   };
 }
