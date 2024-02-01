018a4e73b4c172e645369704912f1d3063b4039e
Michal Korman <michal.korman@icloud.com>
2024-02-01 10:56:12 +0100

    throttling based on response code

--- src/http/throttling.ts
+++ src/http/throttling.ts
@@ -3,6 +3,7 @@ import {RequestHandler} from 'express';
 export type ThrottleOptions = {
   limit?: number;
   windowMs?: number;
+  statusCodes?: number[];
 };
 
 type ClientEntry = {
@@ -22,22 +23,31 @@ export function throttle<TParams, TQuery, TBody, TLocals extends Record<string, 
       return next();
     }
 
-    const entry = entries.get(req.ip);
+    let entry = entries.get(req.ip);
     if (entry && entry.expiresAt >= Date.now()) {
-      entry.hits += 1;
-
-      if (entry.hits > limit) {
+      if (entry.hits >= limit) {
         return res.status(429).json({
           title: 'Request has been throttled',
           type: 'TooManyRequests'
         });
       }
     } else {
-      entries.set(req.ip, {
-        hits: 1,
+      entry = {
+        hits: 0,
         expiresAt: Date.now() + windowMs
-      });
+      };
+      entries.set(req.ip, entry);
     }
+
+    res.on('finish', () => {
+      if (!req.ip) {
+        return;
+      }
+
+      if (!opts?.statusCodes || opts?.statusCodes.includes(res.statusCode)) {
+        entry!.hits += 1;
+      }
+    });
 
     next();
   };
